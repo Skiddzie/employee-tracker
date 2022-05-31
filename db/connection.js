962990d7cd4609a37c35c883332e1ac4701e4684
Skiddzie <aidanthe1@gmail.com>
2022-05-30 23:34:32 -0400

    not working yet

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+// get the client
+const mysql = require('mysql2');
+
+// create the connection to database
+
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: "root",
+  password: "aeonanon",
+  database: 'employee_tracker'
+});
+
+module.exports = connection;
